07bc663a0a361283e84e39d5ed042c76b4131889
mohammed <alu0100777475@ull.edu.es>
2015-11-09 22:46:11 +0000

    last changes
    last changes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 (function(exports){
     "use strict";
 
-    var worker = new Worker('worker.js');
+    var worker = new Worker('js/worker.js');
 
     exports.calculate = function() {
         var value = document.getElementById('original').value;
@@ -10,19 +10,7 @@
 
     worker.addEventListener('message', function(e) {
     var data = e.data.split(" ");
-    /*
-    var value, type;
-    for (var i = 0; i < data.Length/2; i++) {
-        for (var j = 0; j < 2; j++) {
-            value[j] = data[i][j];
-            console.log("value : "+ value);
-            type[j+1] = data[i][j+1];
-            console.log("type : "+ type);
-        }
-    document.getElementById('converted').innerHTML = value[i]+" "+type[i];
-    document.getElementById('converted2').innerHTML = value[j]+" "+type[j];
-    }
-    */
+
     document.getElementById('converted').innerHTML = data[0]+" "+data[1];
     document.getElementById('converted2').innerHTML = data[2]+" "+data[3];
 
